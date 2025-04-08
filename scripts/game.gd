9fa9de02d3538357c1bf0d65553763bff0dbc05e
Francisco Tovar <jtovara@ucenfotec.ac.cr>
2025-04-07 18:51:06 -0600

    main scene
    added menu scene

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,13 @@
 extends Node2D
+@onready var musica: AudioStreamPlayer2D = $Musica
+
+func _ready() -> void:
+	musica.play()
 
 func _process(_delta):
 	#debug actions esc=quit f1=reset
 	if	Input.is_action_just_pressed("quit"):
-		get_tree().quit()
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
 	
